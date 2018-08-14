5787ff13af0064118810fba6caa8b8f968d4d14a
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-08-15 08:36:21 +0900

    Change the setting for electron

--- src/authorizeMedium.js
+++ src/authorizeMedium.js
@@ -12,27 +12,3 @@ const accessToken = process.env.REACT_APP_MEDIUM_API
 client.setAccessToken(accessToken)
 
 export default client
-
-// const url = client.getAuthorizationUrl('secretState', redirectURL, [
-//   medium.Scope.BASIC_PROFILE, medium.Scope.PUBLISH_POST,
-// ])
-
-// // (Send the user to the authorization URL to obtain an authorization code.)
-
-// client.exchangeAuthorizationCode('YOUR_AUTHORIZATION_CODE', redirectURL, (err, token) => {
-//   if (err) console.log(err)
-
-//   client.getUser((err, user) => {
-//     if (err) console.log(err)
-
-//     client.createPost({
-//       userId: user.id,
-//       title: 'A new post',
-//       contentFormat: medium.PostContentFormat.HTML,
-//       content: '<h1>A New Post</h1><p>This is my new post.</p>',
-//       publishStatus: medium.PostPublishStatus.DRAFT,
-//     }, (err, post) => {
-//       console.log(token, user, post)
-//     })
-//   })
-// })
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -16,6 +16,9 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      webSecurity: false,
+    },
   })
 
   // and load the index.html of the app.
